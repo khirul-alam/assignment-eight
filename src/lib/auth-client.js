966045d18ce5d6035e7fa6d64f27fb578765262a
khirul-alam <yeasin1983@gmail.com>
2026-08-09 14:32:39 +0600

    auth client update

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
+
 export const authClient = createAuthClient({
-    
-    baseURL: "http://localhost:3000"
+  baseURL: typeof window !== "undefined" ? window.location.origin : "",
 })
 
-export const { signIn, signUp,signOut, useSession } = createAuthClient()
+export const { signIn, signUp, signOut, useSession } = authClient
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,31 +1,31 @@
-import { betterAuth } from "better-auth";
-import { MongoClient } from "mongodb";
-import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { betterAuth } from 'better-auth'
+import { MongoClient } from 'mongodb'
+import { mongodbAdapter } from 'better-auth/adapters/mongodb'
 
-const client = new MongoClient(process.env.AUTH_DB_URI);
-const db = client.db();
+const client = new MongoClient(process.env.AUTH_DB_URI)
+const db = client.db()
 
 export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL,
 
   emailAndPassword: {
-    enabled: true,
+    enabled: true
   },
 
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    },
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }
   },
 
   database: mongodbAdapter(db, {
-    client,
+    client
   }),
 
   trustedOrigins: [
-    "http://localhost:3000",
-    "https://assignment-eight-eta-nine.vercel.app",
-  ],
-});
+    'http://localhost:3000',
+    'https://assignment-eight-eta-nine.vercel.app'
+  ]
+})
